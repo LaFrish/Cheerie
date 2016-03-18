0126082b1f79c80079d6ba82990af0d00952bf82
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-18 12:07:39 -0400

    planning- erd/psuedocode/wireframe and added other folders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,25 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :get_current_user
-
-  def get_current_user
-    user_id = session[:user_id]
-    if User.exists?(session[:user_id])
-      @current_user = User.find(user_id)
-    else
-      @current_user = nil
-    end
-  end
-
-  def authorized
-    if @current_user
-      return true
-    else
-      flash[:alert] = "You must be signed in to do that!"
-      redirect_to :root
-      return false
-    end
-  end
-
 end
--- app/models/user.rb
+++
@@ -1,4 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :artists
-  has_many :songs
-end
