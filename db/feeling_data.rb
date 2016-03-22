0bd6a6a0bada56c0a602455da853aa03f697e131
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-21 21:26:34 -0400

    trying to add file data

---
+++ db/feeling_data.rb
@@ -0,0 +1,23 @@
+def get_feeling_data
+  feeling_data = {
+    "bored" => {
+                    feeling_name: "Bored"
+                  },
+    "hopeless" => {
+                    feeling_name: "Hopeless"
+                  },
+    "sad" => {
+                    feeling_name: "Sad"
+                  },
+    "frustrated" => {
+                    feeling_name: "Frustrated"
+                  },
+    "angry" => {
+                    feeling_name: "Angry"
+                  }
+    "seriously" => {
+                    feeling_name: "SERIOUSLY!?!"
+                  }
+    }
+  return feeling_data
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require_relative './feeling_data.rb'
+require_relative './cheerup_data.rb'
+
 Feeling.destroy_all
 Cheerup.destroy_all
+
+feeling_data = get_feeling_data()
+cheerup_data = get_cheerup_data()
+
+
+  end
+end
