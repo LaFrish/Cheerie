866616ad98563be679e61381b6fcfbdd7c69b519
YOUR FULL NAME <farishta.haider@gmail.com>
2016-03-24 11:50:50 -0400

    video presentation made

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'lightbox2/rails'
 require 'rails/all'
-require 'sassy-buttons'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,70 +24,88 @@ frustratetag = Tag.create({name: "Frustrated"})
 angertag = Tag.create({name: "Angry"})
 serioustag = Tag.create({name: "Seriously"})
 
-boredimg = Cheerup.create({name: "bored.gif", img_url: "/assets/bored/bored.gif" data-lightbox= "Quotes", data_type: "img", tag: "Bored", feeling: bore})
+boredimg = Cheerup.create({name: "bored_img2.jpg", img_url: "/assets/bored/bored_img2.jpg", data_type: "img", tag: "Bored", feeling: bore})
 
-bored1 = Cheerup.create({name: "bored.gif", img_url: "/assets/bored/bored.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+bored1img = Cheerup.create({name: "bored_img2.jpg", img_url: "/assets/bored/bored_img2.jpg", data_type: "img", tag: "Bored", feeling: bore})
 
-bored2 = Cheerup.create({name: "bored_1.gif", img_url: "/assets/bored/bored_1.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+stressimg = Cheerup.create({name: "stressed_img2.jpg", img_url: "/assets/stressed/stressed_img2.jpg", data_type: "img", tag: "Bored", feeling: bore})
 
-bored3 = Cheerup.create({name: "bored_2.gif", img_url: "/assets/bored/bored_2.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+stress1img = Cheerup.create({name: "stressed_img1.jpg", img_url: "/assets/stressed/stressed_img1.jpg", data_type: "img", tag: "Bored", feeling: bore})
 
-bored4 = Cheerup.create({name: "bored_3.gif", img_url: "/assets/bored/bored_3.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+sadimg = Cheerup.create({name: "sad_img1.jpg", img_url: "/assets/sad/sad_img1.jpg", data_type: "img", tag: "Sad", feeling: bore})
 
-bored5 = Cheerup.create({name: "bored_4.gif", img_url: "/assets/bored/bored_4.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+sad1img = Cheerup.create({name: "sad_img2.jpg", img_url: "/assets/sad/sad_img2.jpg", data_type: "img", tag: "Sad", feeling: bore})
 
-bored6 = Cheerup.create({name: "bored_5.gif", img_url: "/assets/bored/bored_5.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+angerimg = Cheerup.create({name: "angry_img1.jpg", img_url: "/assets/angry/angry_img1.jpg", data_type: "img", tag: "Sad", feeling: bore})
 
-bored_home = Cheerup.create({name: "bored_home.gif", img_url: "/assets/bored/bored_home.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Bored", feeling: bore})
+anger1img = Cheerup.create({name: "angry_img2.jpg", img_url: "/assets/angry/angry_img2.jpg", data_type: "img", tag: "Sad", feeling: bore})
 
-stressed1 = Cheerup.create({name: "stressed_1.gif", img_url: "/assets/stressed/stressed_1.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Stressed",  feeling: stress})
+serious1img = Cheerup.create({name: "seriously_img2.jpg", img_url: "/assets/seriously/seriously_img2.jpg", data_type: "img", tag: "seriously", feeling: bore})
 
-stressed2 = Cheerup.create({name: "stressed_2.gif", img_url: "/assets/stressed/stressed_2.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Stressed",  feeling: stress})
+seriousimg = Cheerup.create({name: "seriously_img1.jpg", img_url: "/assets/seriously/seriously_img1.jpg", data_type: "img", tag: "seriously", feeling: bore})
 
-stressed3 = Cheerup.create({name: "stressed_3.gif", img_url: "/assets/stressed/stressed_3.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Stressed",  feeling: stress})
+bored1 = Cheerup.create({name: "bored.gif", img_url: "/assets/bored/bored.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-stressed4 = Cheerup.create({name: "stressed_4.gif", img_url: "/assets/stressed/stressed_41.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Stressed",  feeling: stress})
+bored2 = Cheerup.create({name: "bored_1.gif", img_url: "/assets/bored/bored_1.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-stressed5 = Cheerup.create({name: "stressed_5.gif", img_url: "/assets/stressed/stressed_5.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Stressed",  feeling: stress})
+bored3 = Cheerup.create({name: "bored_2.gif", img_url: "/assets/bored/bored_2.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-sad1 = Cheerup.create({name: "sad_1.gif", img_url: "/assets/sad/sad_1.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Sadness", feeling: sadness})
+bored4 = Cheerup.create({name: "bored_3.gif", img_url: "/assets/bored/bored_3.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-sad2 = Cheerup.create({name: "sad_2.gif", img_url: "/assets/sad/sad_2.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Sadness", feeling: sadness})
+bored5 = Cheerup.create({name: "bored_4.gif", img_url: "/assets/bored/bored_4.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-sad3 = Cheerup.create({name: "sad_3.gif", img_url: "/assets/sad/sad_3.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Sadness", feeling: sadness})
+bored6 = Cheerup.create({name: "bored_5.gif", img_url: "/assets/bored/bored_5.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-sad4 = Cheerup.create({name: "sad_4.gif", img_url: "/assets/sad/sad_4.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Sadness", feeling: sadness})
+bored_home = Cheerup.create({name: "bored_home.gif", img_url: "/assets/bored/bored_home.gif", data_type: "gif", tag: "Bored", feeling: bore})
 
-sad5 = Cheerup.create({name: "sad_5.gif", img_url: "/assets/sad/sad_5.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Sadness", feeling: sadness})
+stressed1 = Cheerup.create({name: "stressed_1.gif", img_url: "/assets/stressed/stressed_1.gif", data_type: "gif", tag: "Stressed",  feeling: stress})
 
-frustrated1 = Cheerup.create({name: "frustrated_1.gif", img_url: "/assets/frustrated/frustrated_1.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
+stressed2 = Cheerup.create({name: "stressed_2.gif", img_url: "/assets/stressed/stressed_2.gif", data_type: "gif", tag: "Stressed",  feeling: stress})
 
-frustrated2 = Cheerup.create({name: "frustrated_2.gif", img_url: "/assets/frustrated/frustrated_2.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
+stressed3 = Cheerup.create({name: "stressed_3.gif", img_url: "/assets/stressed/stressed_3.gif", data_type: "gif", tag: "Stressed",  feeling: stress})
 
-frustrated3 = Cheerup.create({name: "frustrated_3.gif", img_url: "/assets/frustrated/frustrated_3.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
+stressed4 = Cheerup.create({name: "stressed_4.gif", img_url: "/assets/stressed/stressed_41.gif", data_type: "gif", tag: "Stressed",  feeling: stress})
 
-frustrated4 = Cheerup.create({name: "frustrated_4.gif", img_url: "/assets/frustrated/frustrated_4.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
+stressed5 = Cheerup.create({name: "stressed_5.gif", img_url: "/assets/stressed/stressed_5.gif", data_type: "gif", tag: "Stressed",  feeling: stress})
 
-frustrated5 = Cheerup.create({name: "frustrated_5.gif", img_url: "/assets/frustrated/frustrated_5.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
+sad1 = Cheerup.create({name: "sad_1.gif", img_url: "/assets/sad/sad_1.gif", data_type: "gif", tag: "Sadness", feeling: sadness})
 
-angry1 = Cheerup.create({name: "angry_1.gif", img_url: "/assets/angry/angry_1.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Angry", feeling: anger})
+sad2 = Cheerup.create({name: "sad_2.gif", img_url: "/assets/sad/sad_2.gif", data_type: "gif", tag: "Sadness", feeling: sadness})
 
-angry2 = Cheerup.create({name: "angry_2.gif", img_url: "/assets/angry/angry_2.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Angry", feeling: anger})
+sad3 = Cheerup.create({name: "sad_3.gif", img_url: "/assets/sad/sad_3.gif", data_type: "gif", tag: "Sadness", feeling: sadness})
 
-angry3 = Cheerup.create({name: "angry_3.gif", img_url: "/assets/angry/angry_3.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Angry", feeling: anger})
+sad4 = Cheerup.create({name: "sad_4.gif", img_url: "/assets/sad/sad_4.gif", data_type: "gif", tag: "Sadness", feeling: sadness})
 
-angry4 = Cheerup.create({name: "angry_4.gif", img_url: "/assets/angry/angry_4.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Angry", feeling: anger})
+sad5 = Cheerup.create({name: "sad_5.gif", img_url: "/assets/sad/sad_5.gif", data_type: "gif", tag: "Sadness", feeling: sadness})
 
-angry5 = Cheerup.create({name: "angry_5.gif", img_url: "/assets/angry/angry_5.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Angry", feeling: anger})
+frustrated1 = Cheerup.create({name: "frustrated_1.gif", img_url: "/assets/frustrated/frustrated_1.gif", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
 
-seriously1 = Cheerup.create({name: "seriously_1.gif", img_url: "/assets/seriously/seriously_1.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Seriously", feeling: serious})
+frustrated2 = Cheerup.create({name: "frustrated_2.gif", img_url: "/assets/frustrated/frustrated_2.gif", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
 
-seriously2 = Cheerup.create({name: "seriously_2.gif", img_url: "/assets/seriously/seriously_2.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Seriously", feeling: serious})
+frustrated3 = Cheerup.create({name: "frustrated_3.gif", img_url: "/assets/frustrated/frustrated_3.gif", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
 
-seriously3 = Cheerup.create({name: "seriously_3.gif", img_url: "/assets/seriously/seriously_3.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Seriously", feeling: serious})
+frustrated4 = Cheerup.create({name: "frustrated_4.gif", img_url: "/assets/frustrated/frustrated_4.gif", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
 
-seriously4 = Cheerup.create({name: "seriously_4.gif", img_url: "/assets/seriously/seriously_4.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Seriously", feeling: serious})
+frustrated5 = Cheerup.create({name: "frustrated_5.gif", img_url: "/assets/frustrated/frustrated_5.gif", data_type: "gif", tag: "Frustrated",  feeling: frustrate})
 
-seriously5 = Cheerup.create({name: "seriously_5.gif", img_url: "/assets/seriously/seriously_5.gif" data-lightbox= "GIF Library", data_type: "gif", tag: "Seriously", feeling: serious})
+angry1 = Cheerup.create({name: "angry_1.gif", img_url: "/assets/angry/angry_1.gif", data_type: "gif", tag: "Angry", feeling: anger})
 
-borepost = Post.create({title: "TEST", body: "This is the test of the emergency broadcast system. This is only a test.", img_url: "/assets/bored/bored.gif", data_type: "gif", tag: "Bored"})
+angry2 = Cheerup.create({name: "angry_2.gif", img_url: "/assets/angry/angry_2.gif", data_type: "gif", tag: "Angry", feeling: anger})
+
+angry3 = Cheerup.create({name: "angry_3.gif", img_url: "/assets/angry/angry_3.gif", data_type: "gif", tag: "Angry", feeling: anger})
+
+angry4 = Cheerup.create({name: "angry_4.gif", img_url: "/assets/angry/angry_4.gif", data_type: "gif", tag: "Angry", feeling: anger})
+
+angry5 = Cheerup.create({name: "angry_5.gif", img_url: "/assets/angry/angry_5.gif", data_type: "gif", tag: "Angry", feeling: anger})
+
+seriously1 = Cheerup.create({name: "seriously_1.gif", img_url: "/assets/seriously/seriously_1.gif", data_type: "gif", tag: "Seriously", feeling: serious})
+
+seriously2 = Cheerup.create({name: "seriously_2.gif", img_url: "/assets/seriously/seriously_2.gif", data_type: "gif", tag: "Seriously", feeling: serious})
+
+seriously3 = Cheerup.create({name: "seriously_3.gif", img_url: "/assets/seriously/seriously_3.gif", data_type: "gif", tag: "Seriously", feeling: serious})
+
+seriously4 = Cheerup.create({name: "seriously_4.gif", img_url: "/assets/seriously/seriously_4.gif", data_type: "gif", tag: "Seriously", feeling: serious})
+
+seriously5 = Cheerup.create({name: "seriously_5.gif", img_url: "/assets/seriously/seriously_5.gif", data_type: "gif", tag: "Seriously", feeling: serious})
+
+borepost = Post.create({title: "So BOOORRRREEDDD", body: "This is the longest day ever. I have nothing to do.", img_url: "/assets/bored_15.gif", data_type: "gif", tag: "Bored"})
